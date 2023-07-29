66487c99c34683713a462575119d4715693e12a0
xy <xyzmhx@gmail.com>
2023-07-30 07:11:30 +0900

    first commit

--- dist/main.js
+++ dist/main.js
@@ -4,16 +4,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const translate_1 = require("./translate");
 (function () {
     'use strict';
-    console.log('Plugin started'); // 添加此行来确认插件代码被执行
+    console.log('Plugin started');
     const roomElement = document.getElementById('room-');
     if (roomElement) {
-        console.log('Found room element:', roomElement); // 添加此行来确认roomElement是否找到
+        console.log('Found room element:', roomElement);
         (0, translate_1.translateElement)(roomElement);
     }
     document.addEventListener('DOMNodeInserted', function (e) {
         const targetElement = e.target;
         if (targetElement) {
-            console.log('New node inserted:', targetElement); // 添加此行来确认新节点是否被正确处理
+            console.log('New node inserted:', targetElement);
             (0, translate_1.translateElement)(targetElement);
         }
     });
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { translateElement } from './translate';
 
 (function () {
     'use strict';
-    console.log('Plugin started'); // 添加此行来确认插件代码被执行
+    console.log('Plugin started');
     const roomElement = document.getElementById('room-');
     if (roomElement) {
-        console.log('Found room element:', roomElement); // 添加此行来确认roomElement是否找到
+        console.log('Found room element:', roomElement);
         translateElement(roomElement);
     }
     document.addEventListener('DOMNodeInserted', function (e) {
         const targetElement = e.target as HTMLElement;
         if (targetElement) {
-            console.log('New node inserted:', targetElement); // 添加此行来确认新节点是否被正确处理
+            console.log('New node inserted:', targetElement);
             translateElement(targetElement);
         }
     });
