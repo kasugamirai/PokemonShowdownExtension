db0a84f612054299382e8b2142cb8553c2fdb371
xy <xyzmhx@gmail.com>
2023-07-31 17:20:11 +0900

    Added button text toggling and state persistence using chrome.storage

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,29 @@
 var btnClick = document.getElementById('btnClick');
 
+// Get the translation status from storage and set the button text
+chrome.storage.sync.get('translationActive', function(data) {
+    if (data.translationActive) {
+        btnClick.innerText = "Translation is ON";
+    } else {
+        btnClick.innerText = "Toggle Translation";
+    }
+});
+
 btnClick.addEventListener('click', function() {
     // Send a message to the content script to toggle translation
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {action: "toggleTranslation"});
     });
 
-    // Toggle button text
-    if (btnClick.innerText === "Toggle Translation") {
-        btnClick.innerText = "Translation is OFF";
-    } else {
-        btnClick.innerText = "Toggle Translation";
-    }
+    // Toggle button text and save the new translation status in storage
+    chrome.storage.sync.get('translationActive', function(data) {
+        var newStatus = !data.translationActive;
+        chrome.storage.sync.set({translationActive: newStatus}, function() {
+            if (newStatus) {
+                btnClick.innerText = "Translation is ON";
+            } else {
+                btnClick.innerText = "Toggle Translation";
+            }
+        });
+    });
 });
